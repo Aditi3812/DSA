31c2cf79f20e9f630ff59e9ce02f4acac2e0734c
Aditi Priya <aditipriya3812@gmail.com>
2026-07-25 08:07:33 +0530

    Time: 4 ms (18.81%), Space: 28.4 MB (12%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int>mpp;
+        for(int i = 0; i <nums.size() ;i++)
+        {
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]>nums.size()/2)
+            {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
