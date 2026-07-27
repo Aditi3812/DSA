62b4a35f11fd039f1736799eec10409a07d32792
Aditi Priya <aditipriya3812@gmail.com>
2026-07-27 06:13:17 +0530

    Time: 0 ms (100%), Space: 97.3 MB (61.08%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0];
+        int prof = 0;
+        for(int i = 1 ; i <prices.size(); i++)
+        {
+            if(buy>prices[i]){
+                buy = prices[i];
+            }
+            else{
+                prof = max(prof, prices[i]-buy);
+            }
+        }
+        return prof;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
