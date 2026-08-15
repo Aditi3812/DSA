b6d73f9a33d46756c84be88bfbe2a4d2efcf3dfd
Aditi Priya <aditipriya3812@gmail.com>
2026-08-15 09:38:03 +0530

    Time: 2 ms (36.63%), Space: 17 MB (5.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void solve(vector<int>& ip, int idx, vector<int> &op, vector<vector<int>>&fin)
+     void solve(vector<int> ip, vector<int> op,vector<vector<int>>&fin)
     {
-        if(idx== ip.size()) 
+        if(ip.size() == 0) 
         {
             fin.push_back(op);
             return;
         }
-        solve(ip, idx+1, op,fin);
-        op.push_back(ip[idx]);
-        solve(ip, idx+1, op,fin);
+        vector<int> op1 = op;
+        vector<int> op2 = op;
 
-        op.pop_back();
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        solve(ip,op1,fin);
+        solve(ip,op2,fin);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>fin;
         vector<int>op;
-        solve(nums,0,op,fin);
+        vector<vector<int>>fin;
+        solve(nums,op, fin);
         return fin;
     }
 };
