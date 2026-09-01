b6b862a502a17794a07f940ddc1d3aa9f461118c
Aditi Priya <aditipriya3812@gmail.com>
2026-09-01 08:09:18 +0530

    Time: 67 ms (37.71%), Space: 44.1 MB (12.89%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* root,TreeNode* p, TreeNode* q)
+    {
+        if(root==nullptr)
+        {
+            return nullptr;
+        }
+        if(root==p || root == q)
+        {
+            return root;
+        }
+        TreeNode* l = dfs(root->left, p , q);
+        TreeNode* r = dfs(root->right, p , q);
+
+        if(l==nullptr)
+        {
+            return r;
+        }
+        else if(r==nullptr)
+        {
+            return l;
+        }
+        else
+        {
+            return root;
+        }
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return dfs(root,p,q) ;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
