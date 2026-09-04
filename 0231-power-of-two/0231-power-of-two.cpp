68da123a6ea56b33a3f49e5577b47fa1a42f9d2d
Aditi Priya <aditipriya3812@gmail.com>
2026-09-04 08:50:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.98%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        if(n==1)return true;
+        while(n >1)
+        {
+            if((n&1)==0)
+            {
+                n = n>>1;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
