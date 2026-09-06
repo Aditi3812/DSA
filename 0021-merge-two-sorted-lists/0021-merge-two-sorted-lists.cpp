219354ef75745f6c36ae5d1d4363d5561ef3dd0f
Aditi Priya <aditipriya3812@gmail.com>
2026-09-06 08:00:30 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.66%) - LeetHub - Time: O(n + m), Space: O(n + m)

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void merge(ListNode* list1, ListNode* list2, ListNode* dum)
+    {
+        if(list1==nullptr)
+        {
+            while(list2)
+            {
+                dum->next = list2;
+                list2=list2->next;
+                dum= dum->next;
+            }
+            return;
+        }
+        else if(list2==nullptr)
+        {
+            while(list1)
+            {
+                dum->next = list1;
+                list1=list1->next;
+                dum= dum->next;
+            }
+            return;
+        }
+        if(list1->val<=list2->val)
+        {
+            dum->next = list1;
+            merge(list1->next, list2, dum->next);
+        }
+        else
+        {
+            dum->next = list2;
+            merge(list1, list2->next, dum->next);
+        }
+    }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        ListNode* dum= new ListNode(0);
+        ListNode*temp =nullptr;
+        if(list1->val<=list2->val)
+        {
+            dum->next = list1;
+            merge(list1->next, list2, dum->next);
+        }
+        else
+        {
+            dum->next = list2;
+            merge(list1, list2->next, dum->next);
+        }
+        return dum->next;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
